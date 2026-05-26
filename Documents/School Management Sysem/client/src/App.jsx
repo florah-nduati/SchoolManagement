27f39c6fc2461168594090c5915da78cfa1dcca7
florah <florahnduati68@gmail.com>
2026-05-26 07:44:58 +0300

    remove documents folder completely

--- Documents/School Management Sysem/client/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import AuthApp from "./components/AuthApp.jsx";
-import "./App.css";
-
-function App() {
-  return <AuthApp />;
-}
-
-export default App;
--- Documents/recipe project/client/src/utils/api.js
+++
@@ -1,3 +0,0 @@
-const apiBase = import.meta.env.VITE_API_BASE;
-
-export default apiBase;
